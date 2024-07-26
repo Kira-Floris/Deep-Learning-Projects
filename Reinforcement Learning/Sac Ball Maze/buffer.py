e5090183108387ff55bd340177553e7c5c11f027
Floris Nzabakira <nzafloris@gmail.com>
2024-07-26 15:26:38 +0200

    reinforcement learning - creating agent and unit tests

--- Reinforcement Learning/Sac Ball Maze/buffer.py
+++ Reinforcement Learning/Sac Ball Maze/buffer.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 class ReplayBuffer():
-    def __init__(self, max_size, input_size, n_actions):
+    def __init__(self, max_size, input_shape, n_actions):
         self.mem_size = max_size
         self.mem_ctr = 0
-        self.state_memory = np.zeros((self.mem_size, input_size))
+        self.state_memory = np.zeros((self.mem_size, *input_shape))
         self.next_state_memory = np.zeros((self.mem_size, n_actions))
         self.action_memory = np.zeros((self.mem_size, n_actions))
         self.reward_memory = np.zeros(self.mem_size)
@@ -33,3 +33,8 @@ class ReplayBuffer():
 
         states = self.state_memory[batch]
         next_states = self.state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        return states, actions, rewards, next_states, dones
