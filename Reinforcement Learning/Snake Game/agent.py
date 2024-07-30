ed281aaf646087cf09dcc2e80665ad2e9d7f2acd
Floris Nzabakira <nzafloris@gmail.com>
2024-07-30 11:21:46 +0200

    reinforcement learning - model initialization

--- Reinforcement Learning/Snake Game/agent.py
+++ Reinforcement Learning/Snake Game/agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
 
 MAX_MEMORY = 100000
 BATCH_SIZE = 1000
@@ -13,11 +14,11 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 # parameter to control randomness
-        self.gamma = 0 # discount rate
+        self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft() if exceeded max memory
         self.block_size = 20
-        self.model = None # TODO
-        self.trainer = None # TODO
+        self.model = Linear_QNet(11, 256, 3) # TODO
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma) # TODO
 
         # TODO: model, trainer
 
@@ -92,7 +93,7 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
         return final_move
@@ -130,7 +131,7 @@ def train():
 
             if score > record:
                 record = score
-                # agent.model.save()
+                agent.model.save()
 
             print(f'Game: {agent.n_games}, Score: {score}, Record: {record}')
 
